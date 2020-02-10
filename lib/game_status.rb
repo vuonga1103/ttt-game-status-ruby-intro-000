a5a960e7dd1f5ffa91d42309a08f0ac6b186e764
Anh Vuong <vuonga1103@yahoo.com>
2020-02-10 22:42:26 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,6 +27,7 @@ def won?(board)
 
     if ((position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O"))
       return win_combination
+    else
     end
     return nil
   end
